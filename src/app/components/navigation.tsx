02b6d396a6774de6253bfccb15e8e91588a9cc7e
SineDeveloper <suriya.samp@gmail.com>
2026-02-28 12:19:26 +0700

    nav update

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -6,6 +6,7 @@ export default function navigation() {
             <ul className="main-nav">
                 <li><Link href="/" className="hover:underline">Home</Link></li>
                 <li><Link href="/about" className="hover:underline">About</Link></li>
+                <li><Link href="/blog" className="hover:underline">Blog</Link></li>
             </ul>
         </>
     )
